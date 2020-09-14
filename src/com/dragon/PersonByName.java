2ee54a2f380e4f8f4116ab7f937fa5da3ab49233
Tron <asfedasov@gmail.com>
2020-09-14 14:32:21 +0300

    CC - update 1.0

---
+++ src/com/dragon/PersonByName.java
@@ -0,0 +1,11 @@
+package src.com.dragon;
+
+import java.util.Comparator;
+
+public class PersonByName implements Comparator<Person> {
+    public int compare(Person p1, Person p2) {
+        String s1 = p1.getName();
+        String s2 = p2.getName();
+        return s1.compareTo(s2);
+    }
+}
---
+++ src/com/dragon/PersonSuperComparator.java
@@ -0,0 +1,29 @@
+package src.com.dragon;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class PersonSuperComparator implements Comparator<Person> {
+
+    public static void main(String[] args) {
+        Comparator<Person> personComparator =
+                new PersonByName().thenComparing(new PersonByAge());
+        TreeSet<Person> people = new TreeSet<>(personComparator);
+        people.add(new Person("Александр", 30));
+        people.add(new Person("Александр", 20));
+        people.add(new Person("Виктор", 25));
+        people.add(new Person("Виктор", 35));
+        people.add(new Person("Мирослав", 28));
+        people.add(new Person("Мирослав", 20));
+
+        for (Person p : people) {
+            System.out.println(p.toString());
+        }
+    }
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        return 0;
+    }
+}
+
